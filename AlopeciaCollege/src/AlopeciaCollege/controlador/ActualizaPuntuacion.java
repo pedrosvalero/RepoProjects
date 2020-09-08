6c0aad7934227ed9a64e2446d1e3f464214a00fc
Pedro <pedrosvalero@gmail.com>
2020-09-08 17:12:29 +0200

    Servlet puntos

---
+++ AlopeciaCollege/src/AlopeciaCollege/controlador/ActualizaPuntuacion.java
@@ -0,0 +1,91 @@
+package AlopeciaCollege.controlador;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import AlopeciaCollege.modelo.Usuario;
+import AlopeciaCollege.modelo.UsuarioDAO;
+
+/**
+ * Servlet implementation class ActualizaPuntuacion
+ */
+@WebServlet("/ActualizaPuntuacion")
+public class ActualizaPuntuacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActualizaPuntuacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String dni = request.getParameter("dni");
+		String rol = request.getParameter("rol");
+		int puntuacion = Integer. parseInt(request.getParameter("puntuacion"));
+		UsuarioDAO udao = new UsuarioDAO();
+		Usuario u = null;
+		
+		String destPage = "Home.jsp";
+		
+		try {
+			udao.actualizaPuntos(u);
+			request.setAttribute("dni", u);
+			request.setAttribute("rol", u);
+			request.setAttribute("puntuacion", u);
+			destPage = "Home.jsp";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String dni = request.getParameter("dni");
+		String rol = request.getParameter("rol");
+		int puntuacion = Integer. parseInt(request.getParameter("puntuacion"));
+		
+		Usuario u = new Usuario();
+		u.setDNI(dni);
+		u.setRol(rol);
+		u.setPuntuacion(puntuacion);
+	
+		
+		UsuarioDAO usao = new UsuarioDAO();
+		String pagDest = "Home.jsp";
+	
+
+	try {
+		usao.actualizaPuntos(u);		
+		pagDest = "Home.jsp";
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	RequestDispatcher dispatcher = request.getRequestDispatcher(pagDest);
+	dispatcher.forward(request, response);
+	
+	}
+
+}
